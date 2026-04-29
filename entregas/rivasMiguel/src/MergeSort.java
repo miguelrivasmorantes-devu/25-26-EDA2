3316b771be3b4583e11fbf24f9c9b1f229a4146e
Miguel Rivas Morantes <162994194+miguelrivasmorantes-devu@users.noreply.github.com>
2026-04-29 11:54:16 +0000

    feature/merge sort

--- entregas/rivasMiguel/src/MergeSort.java
+++ entregas/rivasMiguel/src/MergeSort.java
@@ -1,12 +1,44 @@
 public class MergeSort{
-        public static void ordenar(int[] array, int izquierda, int derecha) {
+    public static void ordenar(int[] array, int izquierda, int derecha) {
         if (izquierda >= derecha) {
             return;
         }
         int medio = izquierda + (derecha - izquierda) / 2;
+
+        System.out.print("Array ");
+        for (int i = izquierda; i <= derecha; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.print(" particionado en los grupos: ");
+        for (int i = izquierda; i <= medio; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.print(" Y ");
+        for (int i = medio+1; i <= derecha; i++) {
+            System.out.print(array[i] + " ");
+        }
+
+        System.out.println();
+
         ordenar(array, izquierda, medio);
         ordenar(array, medio + 1, derecha);
         fusionar(array, izquierda, medio, derecha);
+
+        System.out.print("Arrays ");
+        for (int i = izquierda; i <= medio; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.print(" Y ");
+        for (int i = medio+1; i <= derecha; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.print(" fusionados en ");
+        for (int i = izquierda; i <= derecha; i++) {
+            System.out.print(array[i] + " ");
+        }
+
+        System.out.println();
+
     }
 
     public static void ordenarIterativo(int[] array) {
@@ -15,7 +47,40 @@ public class MergeSort{
             for (int izquierda = 0; izquierda < n - tamano; izquierda += 2 * tamano) {
                 int medio = izquierda + tamano - 1;
                 int derecha = Math.min(izquierda + 2 * tamano - 1, n - 1);
+
+                System.out.print("Array ");
+                for (int i = izquierda; i <= derecha; i++) {
+                    System.out.print(array[i] + " ");
+                }
+                System.out.print(" particionado en los grupos: ");
+                for (int i = izquierda; i <= medio; i++) {
+                    System.out.print(array[i] + " ");
+                }
+                System.out.print(" Y ");
+                for (int i = medio+1; i <= derecha; i++) {
+                    System.out.print(array[i] + " ");
+                }
+
+                System.out.println();
+
+
                 fusionar(array, izquierda, medio, derecha);
+                
+
+                System.out.print("Arrays ");
+                for (int i = izquierda; i <= medio; i++) {
+                    System.out.print(array[i] + " ");
+                }
+                System.out.print(" Y ");
+                for (int i = medio+1; i <= derecha; i++) {
+                    System.out.print(array[i] + " ");
+                }
+                System.out.print(" fusionados en ");
+                for (int i = izquierda; i <= derecha; i++) {
+                    System.out.print(array[i] + " ");
+                }
+
+                System.out.println();
             }
         }
     }
